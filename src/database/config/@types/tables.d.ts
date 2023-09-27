6f3fd1b59c4b032bc3f8db7b3d3b6bba8481fb3f
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-09-27 14:15:56 -0300

    declare types of tables knex

---
+++ src/database/config/@types/tables.d.ts
@@ -0,0 +1,14 @@
+import Article from "../../../models/Article";
+import Category from "../../../models/Category";
+import Role from "../../../models/Role";
+import User from "../../../models/User";
+
+declare module "knex/types/tables" {
+	interface Tables {
+		user: User;
+		role: Role;
+		article: Article;
+		category: Category;
+		comment: Comment;
+	}
+}
