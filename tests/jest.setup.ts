b3d79d5de29861d8aff53f33bd20291f60c77417
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-09-28 12:47:10 -0300

    jest setup: run migrations before all e destroy after all

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,6 +1,16 @@
 import supertest from "supertest";
 import server from "../src/server/server";
+import databaseClient from "../src/database/config";
 
 const testServer = supertest(server);
+
+beforeAll(async () => {
+	await databaseClient.migrate.latest();
+	await databaseClient.seed.run();
+});
+
+afterAll(async () => {
+	await databaseClient.destroy();
+});
 
 export default testServer;
