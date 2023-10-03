254039b5052d541ad8cfa2c99819bf886cfcb2ff
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-10-03 12:06:30 -0300

    removed null like return of repository

--- src/repositories/BaseRepository.ts
+++ src/repositories/BaseRepository.ts
@@ -13,22 +13,22 @@ class BaseRepository<T> {
 		return await this.databaseClient(this.tableName).select("*");
 	}
 
-	public async findById(id: number): Promise<T | null> {
+	public async findById(id: number): Promise<T> {
 		return await this.databaseClient(this.tableName).where("id", id).first();
 	}
 
-	public async insert(item: T): Promise<number | null> {
+	public async insert(item: T): Promise<number> {
 		const [result] = await this.databaseClient(this.tableName)
 			.insert(item)
 			.returning("id");
-		return result?.id || null;
+		return result?.id || -1;
 	}
 
-	public async update(id: number, item: T): Promise<T | null> {
+	public async update(id: number, item: T): Promise<boolean> {
 		const updatedCount = await this.databaseClient(this.tableName)
 			.where("id", id)
 			.update(item);
-		return updatedCount > 0 ? await this.findById(id) : null;
+		return updatedCount > 0;
 	}
 
 	public async delete(id: number): Promise<boolean> {
