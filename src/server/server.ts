e810a17e05c0c7dbd820dcfab6858b45170aac13
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-10-04 13:37:20 -0300

    server use role routes

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import roleRoutes from "./routes/roleRoutes";
 
 const server = express();
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.use("/role", roleRoutes);
 
 export default server;
